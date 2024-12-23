1984e772e15a83cbbe2c039fb3d675b10bdc3951
Caohung77 <caohungnguyen@gmail.com>
2024-12-23 23:58:23 +0100

    style: update profile picture and LinkedIn button to use brand orange color

--- src/App.tsx
+++ src/App.tsx
@@ -483,8 +483,8 @@ function App() {
                               <img 
                                 src="/chn-profile.gif" 
                                 alt="Cao Hung Nguyen" 
-                                className="is-rounded"
-                                style={{ border: '3px solid #3298dc' }}
+                                className="is-rounded profile-image"
+                                style={{ border: '3px solid var(--primary-color)' }}
                               />
                             </figure>
                             <h3 className="title is-4 mt-3 text-gray-800">Cao Hung Nguyen</h3>
@@ -499,14 +499,14 @@ function App() {
                                   href="https://www.linkedin.com/in/caohungnguyen/"
                                   target="_blank"
                                   rel="noopener noreferrer"
-                                  className="button is-info is-medium is-rounded is-fullwidth-mobile"
+                                  className="button is-linkedin is-medium is-rounded is-fullwidth-mobile"
                                 >
                                   <span className="icon">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                                       <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
                                     </svg>
                                   </span>
-                                  <span>Auf LinkedIn verbinden</span>
+                                  <span>LinkedIn verbinden</span>
                                 </a>
                               </div>
                             </div>
